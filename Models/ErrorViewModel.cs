73b3d49fb4e1807b42607f8a92d153ade961689c
Riaan Carelse <ST10065550@vcconnect.edu.za>
2025-05-06 13:25:09 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agri_Energy_Riaan_Carelse_ST10065550_Prog7311_PoePart2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
